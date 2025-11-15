3d03132257ef49f42c58649b98d1420e2ba97fc2
TuanLee <23521711@gm.uit.edu.vn>
2025-11-15 20:53:38 +0700

    Add class sprite into project

--- GameKillingDemonn/GameKillingDemonn/GameForm.cs
+++ GameKillingDemonn/GameKillingDemonn/GameForm.cs
@@ -22,4 +22,64 @@ namespace GameKillingDemonn
 
         }
     }
+
+    public class Sprite
+    {
+        public Bitmap Sheet;
+        public int FrameW, FrameH;
+
+        public int CurFrame = 0;
+        public int TotalFrames;
+
+        public int FrameInterval = 80;
+        int timer = 0;
+
+        public bool FlipX = false;
+
+        public Sprite(string path, int frameW, int frameH)
+        {
+            Sheet = new Bitmap(path);
+            FrameW = frameW;
+            FrameH = frameH;
+            TotalFrames = Sheet.Width / frameW;
+        }
+
+        public void Reset()
+        {
+            CurFrame = 0;
+            timer = 0;
+        }
+
+        public void Update(int dt)
+        {
+            timer += dt;
+            if (timer >= FrameInterval)
+            {
+                timer -= FrameInterval;
+                CurFrame++;
+
+                if (CurFrame >= TotalFrames)
+                    CurFrame = TotalFrames - 1; // animation dead/attack đứng ở cuối
+            }
+        }
+
+        public void Draw(Graphics g, float x, float y)
+        {
+            Rectangle src = new Rectangle(CurFrame * FrameW, 0, FrameW, FrameH);
+
+            if (!FlipX)
+            {
+                g.DrawImage(Sheet, x, y, src, GraphicsUnit.Pixel);
+            }
+            else
+            {
+                g.TranslateTransform(x + FrameW, y);
+                g.ScaleTransform(-1, 1);
+                g.DrawImage(Sheet, 0, 0, src, GraphicsUnit.Pixel);
+                g.ResetTransform();
+            }
+        }
+    }
+
+   
 }
